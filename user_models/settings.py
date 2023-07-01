ba05df28bcd8f70515fad293eacf16faa849664b
Rahul <rahulsmrd@gmail.com>
2023-07-01 11:38:08 +0530

    changed

--- user_models/settings.py
+++ user_models/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m!xjsamim_o%cf3@e(o)-s7k3l@=x*kv99&^=1e^_94+1)njh4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".vercel.app"]
 
 
 # Application definition
