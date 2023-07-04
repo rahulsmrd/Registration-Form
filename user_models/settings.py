94915931a162128e40feb054d9e4757d8eabe827
Rahul <rahulsmrd@gmail.com>
2023-07-04 13:36:34 +0530

    changed

--- user_models/settings.py
+++ user_models/settings.py
@@ -32,7 +32,6 @@ ALLOWED_HOSTS = [".vercel.app",'127.0.0.1']
 # Application definition
 
 INSTALLED_APPS = [
-    'whitenoise.runserver_nostatic'
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
